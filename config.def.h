d49f0bff143d375b16f2a6f69c52878fbe0b41b5
Evan Porter <evanwporter@gmail.com>
2026-08-28 16:07:03 -0700

    patch: restartsig
    * https://dwm.suckless.org/patches/restartsig/
    * restart key is MOD + SHIFT + C to restart

--- config.def.h
+++ config.def.h
@@ -123,6 +123,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      quit,           {.i = 1} },
 };
 
 /* button definitions */
