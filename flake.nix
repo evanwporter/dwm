9f0248deebd3d3cc986f846476f03874dd6c4311
Evan Porter <evanwporter@gmail.com>
2026-08-28 21:19:25 -0700

    format flake.nix
    format flake

--- flake.nix
+++ flake.nix
@@ -1,52 +1,53 @@
 {
-  description = "dwm development workspace";
+	description = "dwm development workspace";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "dwm";
-            version = "6.8";
-            src = self;
+	outputs = {
+		self,
+		nixpkgs,
+	}: let
+		supportedSystems = ["x86_64-linux" "aarch64-linux"];
+		forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+	in {
+		packages =
+			forAllSystems (system: let
+					pkgs = nixpkgs.legacyPackages.${system};
+				in {
+					default =
+						pkgs.stdenv.mkDerivation {
+							pname = "dwm";
+							version = "6.8";
+							src = self;
 
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            buildInputs = with pkgs; [
-              fontconfig
-              freetype
-              libx11
-              libxft
-              libxinerama
-            ];
+							nativeBuildInputs = [pkgs.pkg-config];
+							buildInputs = with pkgs; [
+								fontconfig
+								freetype
+								libx11
+								libxft
+								libxinerama
+							];
 
-            installFlags = [ "PREFIX=$(out)" ];
-          };
-        });
+							installFlags = ["PREFIX=$(out)"];
+						};
+				});
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            inputsFrom = [ self.packages.${system}.default ];
-            packages = with pkgs; [
-              bear
-              clang-tools
-              gnumake
-              jq
-              pkg-config
-            ];
-          };
-        });
-    };
+		devShells =
+			forAllSystems (system: let
+					pkgs = nixpkgs.legacyPackages.${system};
+				in {
+					default =
+						pkgs.mkShell {
+							inputsFrom = [self.packages.${system}.default];
+							packages = with pkgs; [
+								bear
+								clang-tools
+								gnumake
+								jq
+								pkg-config
+							];
+						};
+				});
+	};
 }
