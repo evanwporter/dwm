867323c26bfe5ca1ec3b0826c5a95c9213659339
Evan Porter <evanwporter@gmail.com>
2026-08-28 18:44:39 -0700

    change fonts

--- config.def.h
+++ config.def.h
@@ -10,8 +10,11 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray             = 1;   /* 0 means no systray */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 0;   /* 0 means bottom bar */
-static const char *fonts[]               = { "monospace:size=10" };
-static const char dmenufont[]            = "monospace:size=10";
+static const char *fonts[]               = { 
+    "JetBrainsMonoNL NFP:size=13:style=Bold", 
+    "Font Awesome 6 Free Solid:size=13"
+};
+static const char dmenufont[]            = "JetBrainsMonoNL NFP:size=20:style=Bold";
 
 static char normfgcolor[]                = "#bbbbbb";
 static char normbgcolor[]                = "#222222";
