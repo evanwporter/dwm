7f8fb1f2a2a0a4303e50dd2fbfea9659df733ce7
Evan Porter <evanwporter@gmail.com>
2026-09-14 15:12:09 -0700

    use `dmenu_desktop_run` command instead
    add `-i` flag to make dmenu case insensitive

--- config.def.h
+++ config.def.h
@@ -89,7 +89,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { 
-    "dmenu_run", 
+    "dmenu_desktop_run",
+    "-i",
     "-m", dmenumon, 
     "-fn", dmenufont, 
     "-nb", normbgcolor,
